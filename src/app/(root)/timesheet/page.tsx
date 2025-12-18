8c16c73dd8391599e28e0ea7977e85febccfd135
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-18 11:25:40 +0700

    init: timesheet page

---
+++ src/app/(root)/timesheet/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Time Sheet - CN Timesheet',
+  description: 'CN Timesheet',
+};
+
+const TimeSheetPage = () => {
+  return <div>Time Sheet Page</div>;
+};
+
+export default TimeSheetPage;
