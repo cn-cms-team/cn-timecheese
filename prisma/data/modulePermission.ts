482be0e3513f6e638b554e107526570c922c69d1
keerati.y <keerati.y@clicknext.co.th>
2025-12-18 15:42:06 +0700

    feat: add initial data models and seed script for user, role, permission, and module management

---
+++ prisma/data/modulePermission.ts
@@ -0,0 +1,170 @@
+import { EPermissions } from '../../src/lib/constants/pms';
+import { EModules } from '../../src/lib/constants/module';
+
+export const modulePermission = [
+  {
+    module_code: EModules.DASHBOARD,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.REPORT_PROJECT_STATUS,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.REPORT_PROJECT_STATUS,
+    pms_code: EPermissions.EXPORT,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.CREATE,
+    order: 2,
+  },
+  {
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.EDIT,
+    order: 3,
+  },
+  {
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.DELETE,
+    order: 4,
+  },
+  {
+    module_code: EModules.ADMIN_ACTIVITY_LOG,
+    pms_code: EPermissions.VIEW,
+    order: 1,
+  },
+  {
+    module_code: EModules.ADMIN_ACTIVITY_LOG,
+    pms_code: EPermissions.EXPORT,
+    order: 2,
+  },
+];
---
+++ prisma/data/rolePermission.ts
@@ -0,0 +1,236 @@
+import { EPermissions } from '../../src/lib/constants/pms';
+import { EModules } from '../../src/lib/constants/module';
+
+export const rolePermissionAdmin = [
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.DASHBOARD,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.REPORT_PROJECT_STATUS,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.REPORT_PROJECT_STATUS,
+    pms_code: EPermissions.EXPORT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_USER,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_ROLE,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_PROJECT,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TASK_TYPE,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_TEAM,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_POSITION,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_ACTIVITY_LOG,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '3f6a9e2c-8c4d-4c9b-a2d5-9b2c7e1a4f63',
+    module_code: EModules.ADMIN_ACTIVITY_LOG,
+    pms_code: EPermissions.EXPORT,
+  },
+];
+
+export const rolePermissionMember = [
+  {
+    role_id: 'a91d0f7e-2b6e-4e9f-9c4a-6d3b1f8a52e7',
+    module_code: EModules.DASHBOARD,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: 'a91d0f7e-2b6e-4e9f-9c4a-6d3b1f8a52e7',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: 'a91d0f7e-2b6e-4e9f-9c4a-6d3b1f8a52e7',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: 'a91d0f7e-2b6e-4e9f-9c4a-6d3b1f8a52e7',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: 'a91d0f7e-2b6e-4e9f-9c4a-6d3b1f8a52e7',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.DELETE,
+  },
+];
+
+export const rolePermissionPM = [
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.DASHBOARD,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.CREATE,
+  },
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.EDIT,
+  },
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.TIME_SHEET,
+    pms_code: EPermissions.DELETE,
+  },
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.REPORT_PROJECT_STATUS,
+    pms_code: EPermissions.VIEW,
+  },
+  {
+    role_id: '5e2c8b41-7a9d-4f6c-b0e3-1d9a6c4f28b5',
+    module_code: EModules.REPORT_PROJECT_STATUS,
+    pms_code: EPermissions.EXPORT,
+  },
+];
