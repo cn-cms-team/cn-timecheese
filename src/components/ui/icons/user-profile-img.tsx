8801bd416ac4b33f75e2f7acf7d52af1625e4f82
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-18 16:32:04 +0700

    fix: user profile img attribute svg

--- src/components/ui/icons/user-profile-img.tsx
+++ src/components/ui/icons/user-profile-img.tsx
@@ -22,21 +22,21 @@ const UserProfileImage = ({ width = 80, height = 80 }: IProps) => {
         rx="39.25"
         fill="white"
         stroke="black"
-        stroke-width="1.5"
+        strokeWidth="1.5"
       />
       <path
         d="M55.9508 60.5079V55.9506C55.9508 53.5332 54.9906 51.2149 53.2812 49.5056C51.5719 47.7962 49.2536 46.8359 46.8362 46.8359H33.1642C30.7469 46.8359 28.4285 47.7962 26.7192 49.5056C25.0099 51.2149 24.0496 53.5332 24.0496 55.9506V60.5079"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M40.0002 37.7212C45.0341 37.7212 49.1149 33.6405 49.1149 28.6066C49.1149 23.5727 45.0341 19.4919 40.0002 19.4919C34.9663 19.4919 30.8856 23.5727 30.8856 28.6066C30.8856 33.6405 34.9663 37.7212 40.0002 37.7212Z"
         stroke="black"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
