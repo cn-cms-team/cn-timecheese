dca957fd7f744aae6c5d9f90a5b699c70272964f
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-22 17:34:16 +0700

    add: schema time sheet

---
+++ src/components/pages/timesheet/schema.ts
@@ -0,0 +1,14 @@
+import z from 'zod';
+
+const schema = z.object({
+  project_id: z.string().nonempty('กรุณาเลือกโปรเจค'),
+  task_type_id: z.string().nonempty('กรุณาเลือกประเภทงาน'),
+  start_date: z.date('กรุณาเลือกวันที่เริ่มต้น'),
+  end_date: z.date('กรุณาเลือกวันที่สิ้นสุด'),
+  detail: z.string().nonempty('กรุณากรอกรายละเอียดการทำงาน'),
+  remark: z.string().optional(),
+});
+
+type Schema = z.infer<typeof schema>;
+
+export { schema as timesheetCreateEditSchema, type Schema as TimesheetCreateEditSchema };
