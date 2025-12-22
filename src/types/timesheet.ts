6ed5bf214d909aca6efff5ed173720fb072af38a
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-22 17:34:37 +0700

    add: time sheet interface response request

---
+++ src/types/timesheet.ts
@@ -0,0 +1,21 @@
+export interface ITimeSheetResponse {
+  id: string;
+  project_id: string;
+  project_name: string;
+  task_type_id: string;
+  task_type_name: string;
+  start_date: string;
+  end_date: string;
+  detail: string;
+  remark?: string;
+  created_at: string;
+}
+
+export interface ITimeSheetRequest {
+  project_id: string;
+  task_type_id: string;
+  start_date: string;
+  end_date: string;
+  detail: string;
+  remark?: string;
+}
