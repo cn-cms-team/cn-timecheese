8f7b5dd931ac959a23b2dbde55a152bad648664e
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-23 12:07:18 +0700

    fix: min-height for card

--- src/components/pages/timesheet/timesheet-event-card.tsx
+++ src/components/pages/timesheet/timesheet-event-card.tsx
@@ -1,7 +1,35 @@
 'use client';
 
-const TimeSheetEventCard = () => {
-  return <div className="w-full h-full p-2 rounded-lg bg-primary shadow-lg">Event Card</div>;
+import { buddhistFormatDate } from '@/lib/functions/date-format';
+import { ITimeSheetResponse } from '@/types/timesheet';
+
+interface IProps {
+  data?: ITimeSheetResponse;
+  height: number;
+}
+
+const TimeSheetEventCard = ({ data = undefined, height }: IProps) => {
+  if (!data) return;
+  return (
+    <div className="w-full min-h-full px-4 py-3 rounded-lg bg-primary shadow-lg cursor-pointer">
+      <header>
+        <h3 className="font-bold text-lg">{data.project_name}</h3>
+        <p className="font-semibold text-wrap overflow-hidden truncate">{data.task_type_name}</p>
+      </header>
+      <main>
+        <div>
+          <span>{buddhistFormatDate(data.start_date, 'HH:ii น.')}</span>
+          {' - '}
+          <span>{buddhistFormatDate(data.end_date, 'HH:ii น.')}</span>
+        </div>
+        {height >= 100 && (
+          <div className="overflow-hidden">
+            <p className="text-sm text-neutral-800 truncate">{data.detail}</p>
+          </div>
+        )}
+      </main>
+    </div>
+  );
 };
 
 export default TimeSheetEventCard;
