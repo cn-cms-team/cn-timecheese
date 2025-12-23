23b6bb98d5229cfed2ebf577bc7f1839504cb90d
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-23 12:09:06 +0700

    fix: task detail layout

--- src/components/pages/timesheet/timesheet-task-detail.tsx
+++ src/components/pages/timesheet/timesheet-task-detail.tsx
@@ -13,26 +13,30 @@ interface IProps {
 
 const TimeSheetdataDetail = ({ data, close, setIsPopoverEdit }: IProps) => {
   return (
-    <div className="grid grid-cols-1">
-      <header className="flex items-center justify-end">
-        <Button
-          className="bg-transparent border-transparent hover:bg-transparent cursor-pointer p-2 focus:border-none float-right"
-          onClick={() => setIsPopoverEdit(true)}
-        >
-          <SquarePen stroke="#000" strokeWidth={2} />
-        </Button>
-        <Button
-          className="bg-transparent border-transparent hover:bg-transparent cursor-pointer p-2 focus:border-none float-right"
-          onClick={() => {
-            close();
-            setIsPopoverEdit(false);
-          }}
-        >
-          <X stroke="#000" strokeWidth={2} />
-        </Button>
+    <div className="grid grid-cols-1 p-4">
+      <header className="flex items-center justify-between">
+        <div className="truncate">
+          <h3 className="font-bold text-lg">{data.project_name}</h3>
+        </div>
+        <div className="flex items-center">
+          <Button
+            className="bg-transparent border-transparent hover:bg-transparent cursor-pointer p-2 focus:border-none float-right"
+            onClick={() => setIsPopoverEdit(true)}
+          >
+            <SquarePen stroke="#000" strokeWidth={2} />
+          </Button>
+          <Button
+            className="bg-transparent border-transparent hover:bg-transparent cursor-pointer p-2 focus:border-none float-right"
+            onClick={() => {
+              close();
+              setIsPopoverEdit(false);
+            }}
+          >
+            <X stroke="#000" strokeWidth={2} />
+          </Button>
+        </div>
       </header>
-      <main className="">
-        <h3 className="font-bold text-lg">{data.project_name}</h3>
+      <main>
         <p>{data.task_type_name}</p>
         <div className="text-sm">
           <span>{buddhistFormatDate(data.start_date, 'HH:ii น.')}</span>
@@ -40,7 +44,7 @@ const TimeSheetdataDetail = ({ data, close, setIsPopoverEdit }: IProps) => {
           <span>{buddhistFormatDate(data.end_date, 'HH:ii น.')}</span>
         </div>
         <div className="text-sm">
-          <p className="text-sm text-black">{data.detail}</p>
+          <p className="text-sm text-black whitespace-pre-wrap">{data.detail}</p>
         </div>
       </main>
     </div>
