8cc9221f089c8a99d32ff29505a6318aa70cc941
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-24 16:23:01 +0700

    fix: change validation rule of task_type_id and project_id

--- src/components/pages/timesheet/schema.ts
+++ src/components/pages/timesheet/schema.ts
@@ -2,11 +2,23 @@ import z from 'zod';
 
 const schema = z.object({
   id: z.string().optional(),
-  project_id: z.string().nonempty('กรุณาเลือกโปรเจค'),
-  task_type_id: z.string().nonempty('กรุณาเลือกประเภทงาน'),
+  project_id: z
+    .string()
+    .optional()
+    .refine((val) => !!val && val !== 'none', {
+      message: 'กรุณาเลือกโปรเจค',
+    }),
+
+  task_type_id: z
+    .string()
+    .optional()
+    .refine((val) => !!val && val !== 'none', {
+      message: 'กรุณาเลือกประเภทงาน',
+    }),
+
   stamp_date: z.date('กรุณาเลือกวันที่บันทึกเวลา'),
-  start_date: z.date('กรุณาเลือกวันที่เริ่มต้น'),
-  end_date: z.date('กรุณาเลือกวันที่สิ้นสุด'),
+  start_date: z.date('กรุณาเลือกกรอกเริ่มต้น'),
+  end_date: z.date('กรุณากรอกวันที่สิ้นสุด'),
   detail: z.string().nonempty('กรุณากรอกรายละเอียดการทำงาน'),
   remark: z.string().optional(),
 });
