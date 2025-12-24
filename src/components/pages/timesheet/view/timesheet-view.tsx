eea676c39f13b2f5571506f6d089bb69304924fe
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-12-24 16:40:48 +0700

    fix: use module layout

--- src/components/pages/timesheet/view/timesheet-view.tsx
+++ src/components/pages/timesheet/view/timesheet-view.tsx
@@ -3,21 +3,22 @@
 import TimeSheetHeader from '../header/timesheet-header';
 import TimeSheetProvider from './timesheet-context';
 import TimeSheetCalendarBody from '../timesheet-calendar-body';
-import { Header, HeaderTitle } from '@/components/ui/custom/header';
 import TimeSheetPeriodSelection from '../header/timesheet-period-selection';
+import ModuleLayout from '@/components/layouts/ModuleLayout';
 
 const TimeSheetView = () => {
   return (
-    <TimeSheetProvider>
-      <div className="px-4 w-full noto-sans">
-        <Header>
-          <HeaderTitle title="Time Sheet" />
-        </Header>
-        <TimeSheetHeader />
-        <TimeSheetPeriodSelection />
-        <TimeSheetCalendarBody />
-      </div>
-    </TimeSheetProvider>
+    <ModuleLayout
+      headerTitle={'Time Sheet'}
+      headerButton={null}
+      content={
+        <TimeSheetProvider>
+          <TimeSheetHeader />
+          <TimeSheetPeriodSelection />
+          <TimeSheetCalendarBody />
+        </TimeSheetProvider>
+      }
+    />
   );
 };
 
