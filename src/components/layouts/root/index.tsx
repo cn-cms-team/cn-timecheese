63dd08da52009c6fe1b5feca1a647e8033c079e6
CLICKNEXT\Chitsanupong.m <csnpboss@gmail.com>
2025-12-25 13:26:01 +0700

    feat: add layout toaster

--- src/components/layouts/root/index.tsx
+++ src/components/layouts/root/index.tsx
@@ -3,6 +3,7 @@ import { AppSidebar } from './app-sidebar';
 import { SessionProvider } from 'next-auth/react';
 import { auth } from '@/auth';
 import { AppProvider } from '@/components/context/app-context';
+import { Toaster } from '@/components/ui/sonner';
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
@@ -13,6 +14,7 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
         <SidebarProvider>
           <AppSidebar />
           <main className="w-full overflow-auto bg-stone-50">{children}</main>
+          <Toaster />
         </SidebarProvider>
       </AppProvider>
     </SessionProvider>
