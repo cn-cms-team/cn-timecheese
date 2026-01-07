2ce930b41b4dc9f64d3aad3344ed62f856190050
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-01-07 15:46:49 +0700

    feat: enhance DashboardBarChart component with data fetching and series calculation

--- src/components/pages/dashboard/dashboard-bar-chart.tsx
+++ src/components/pages/dashboard/dashboard-bar-chart.tsx
@@ -1,16 +1,58 @@
 'use client';
+import { useEffect, useMemo, useState } from 'react';
 
+import { fetcher } from '@/lib/fetcher';
+import { IDashboardAttendance } from '@/types/dashboard';
+
+import Dropdown from '@/components/ui/custom/input/dropdown';
 import ApexChart from '@/components/ui/custom/chart/apex-chart';
 import { useDashboardContext } from './view/dashboard-use-context';
-import Dropdown from '@/components/ui/custom/input/dropdown';
 
 const DashboardBarChart = () => {
-  const { barchartOption, monthOption, selectedMonth, setSelectedMonth } = useDashboardContext();
+  const prefix = process.env.NEXT_PUBLIC_APP_URL;
+  const { barchartOption, monthOption, selectedMonth, weekDays, setSelectedMonth } =
+    useDashboardContext();
+
+  const [data, setData] = useState<IDashboardAttendance[]>([]);
+
   const handleSelectMonth = (month: number) => {
     if (setSelectedMonth) {
       setSelectedMonth(month);
     }
   };
+
+  const fetchData = async () => {
+    try {
+      const res = await fetcher<IDashboardAttendance[]>(
+        `${prefix}/api/v1/dashboard/attendance?month=${selectedMonth}`
+      );
+
+      setData(res);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (selectedMonth === null) return;
+
+    fetchData();
+  }, [selectedMonth]);
+
+  const series = useMemo(() => {
+    const dateMap = new Map(
+      data.map((item) => [new Date(item.date).getDate(), item.total_seconds])
+    );
+
+    return [
+      {
+        data: weekDays.map((day) => {
+          const totalSeconds = dateMap.get(Number(day));
+          return totalSeconds ? totalSeconds / 3600 : 0;
+        }),
+      },
+    ];
+  }, [data, weekDays]);
 
   return (
     <div className="w-full border rounded-md p-3">
@@ -30,7 +72,7 @@ const DashboardBarChart = () => {
           />
         </div>
         <div className="min-w-150 overflow-x-auto">
-          <ApexChart options={barchartOption} type="bar" height={250} />
+          <ApexChart options={barchartOption} series={series} type="bar" height={250} />
         </div>
       </main>
     </div>
