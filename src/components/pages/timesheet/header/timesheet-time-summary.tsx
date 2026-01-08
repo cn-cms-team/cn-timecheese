eb486287b8f032cd7cde5bee75608ec0108e6ae7
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-01-08 11:13:03 +0700

    feat: add rounded corners to timesheet time summary container

--- src/components/pages/timesheet/header/timesheet-time-summary.tsx
+++ src/components/pages/timesheet/header/timesheet-time-summary.tsx
@@ -6,7 +6,7 @@ const TimeSheetTimeSummary = () => {
   const { userInfo } = useTimeSheetContext();
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
-      <div className="flex flex-col justify-center gap-2 bg-white p-2 md:col-span-2 col-span-1 text-center">
+      <div className="flex flex-col justify-center gap-2 bg-white p-2 md:col-span-2 col-span-1 text-center rounded-md ">
         <span>เวลาทั้งหมด</span>
         <span className="text-black">
           {userInfo?.total_tracked_hr} <span className="text-gray-300">ชั่วโมง</span>
