40d28fc1af29c4246a6f1c1b46ff8652e768a8a3
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2026-01-08 13:49:43 +0700

    fix: hide calendar picker indicator in TimeInput component

--- src/components/ui/custom/input/time-input.tsx
+++ src/components/ui/custom/input/time-input.tsx
@@ -21,7 +21,7 @@ const TimeInput = ({
         type="time"
         {...props}
         className={cn(
-          '[&::-webkit-calendar-picker-indicator]:opacity-0 [&::-webkit-calendar-picker-indicator]:absolute [&::-webkit-calendar-picker-indicator]:right-3 [&::-webkit-calendar-picker-indicator]:top-1/2 [&::-webkit-calendar-picker-indicator]:-translate-y-1/2',
+          '[&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none',
           isError && 'border-destructive focus-visible:border-destructive '
         )}
       />
