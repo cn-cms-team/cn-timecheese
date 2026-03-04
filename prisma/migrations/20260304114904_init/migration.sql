7c10d41b053f99143cae41172be7c8a3fe887780
keerati.y <keerati.y@clicknext.co.th>
2026-03-04 18:49:41 +0700

    Refactor database schema and migrations to support PostgreSQL
    - Updated team.json with new UUIDs for teams.
    - Removed initial migration SQL file and added new migration files for PostgreSQL.
    - Adjusted task type codes in migration files to include additional types.
    - Created new migration for initializing database schema with PostgreSQL types and constraints.
    - Changed database provider from MySQL to PostgreSQL in migration lock and schema files.
    - Updated seed script to use PostgreSQL adapter.
    - Modified Prisma client initialization to use PostgreSQL adapter.

---
+++ prisma/migrations/20260304114904_init/migration.sql
@@ -0,0 +1,313 @@
+-- CreateEnum
+CREATE TYPE "TaskTypeCode" AS ENUM ('DEVELOPMENT', 'BUSINESS', 'OPERATION', 'SALES', 'QA', 'IT', 'OTHER', 'COMPANY', 'LEAVE', 'HR', 'ACCOUNTING', 'GRAPHIC_DESIGN', 'MEDIA');
+
+-- CreateEnum
+CREATE TYPE "ProjectStatus" AS ENUM ('PRE_SALE', 'PLANNING', 'IN_PROGRESS', 'MAINTENANCE', 'ON_HOLD', 'CANCELLED', 'TERMINATED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "code" VARCHAR(50) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(100) NOT NULL,
+    "last_name" VARCHAR(100) NOT NULL,
+    "nick_name" VARCHAR(100),
+    "start_date" TIMESTAMP(3),
+    "end_date" TIMESTAMP(3),
+    "last_login_at" TIMESTAMP(3),
+    "salary_range" VARCHAR(100),
+    "reset_password_date" TIMESTAMP(3),
+    "position_level_id" UUID,
+    "team_id" UUID,
+    "role_id" UUID,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" UUID NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "updated_by" UUID,
+    "is_active" BOOLEAN NOT NULL DEFAULT false,
+    "is_enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "roles" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" UUID NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "updated_by" UUID,
+    "is_enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "role_permissions" (
+    "role_id" UUID NOT NULL,
+    "module_code" VARCHAR(30) NOT NULL,
+    "pms_code" VARCHAR(10) NOT NULL,
+
+    CONSTRAINT "role_permissions_pkey" PRIMARY KEY ("role_id","module_code","pms_code")
+);
+
+-- CreateTable
+CREATE TABLE "m_permissions" (
+    "code" VARCHAR(10) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "order" INTEGER NOT NULL,
+
+    CONSTRAINT "m_permissions_pkey" PRIMARY KEY ("code")
+);
+
+-- CreateTable
+CREATE TABLE "m_modules" (
+    "code" VARCHAR(30) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "parent_code" VARCHAR(30),
+    "order" INTEGER NOT NULL,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "m_modules_pkey" PRIMARY KEY ("code")
+);
+
+-- CreateTable
+CREATE TABLE "m_modules_permissions" (
+    "module_code" VARCHAR(30) NOT NULL,
+    "pms_code" VARCHAR(10) NOT NULL,
+    "order" INTEGER NOT NULL,
+
+    CONSTRAINT "m_modules_permissions_pkey" PRIMARY KEY ("module_code","pms_code")
+);
+
+-- CreateTable
+CREATE TABLE "teams" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" UUID NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "updated_by" UUID,
+    "is_enabled" BOOLEAN NOT NULL DEFAULT true,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "teams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "team_leaders" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "team_id" UUID NOT NULL,
+
+    CONSTRAINT "team_leaders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "positions" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" UUID NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "updated_by" UUID,
+    "is_enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "positions_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "position_levels" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255),
+    "position_id" UUID NOT NULL,
+    "ord" INTEGER,
+    "is_enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "position_levels_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "task_types" (
+    "id" UUID NOT NULL,
+    "type" "TaskTypeCode" NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255),
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "task_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "code" VARCHAR(25),
+    "description" VARCHAR(255),
+    "value" DOUBLE PRECISION,
+    "people_cost_percent" DOUBLE PRECISION,
+    "people_cost" DOUBLE PRECISION,
+    "start_date" TIMESTAMP(3),
+    "end_date" TIMESTAMP(3),
+    "status" "ProjectStatus" NOT NULL DEFAULT 'IN_PROGRESS',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_by" UUID NOT NULL,
+    "updated_at" TIMESTAMP(3),
+    "updated_by" UUID,
+    "is_enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "project_members" (
+    "project_id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "role" VARCHAR(100) NOT NULL,
+    "day_price" DOUBLE PRECISION,
+    "hour_price" DOUBLE PRECISION,
+    "work_hours" INTEGER NOT NULL,
+    "start_date" DATE,
+    "end_date" DATE,
+
+    CONSTRAINT "project_members_pkey" PRIMARY KEY ("project_id","user_id")
+);
+
+-- CreateTable
+CREATE TABLE "project_task_types" (
+    "id" UUID NOT NULL,
+    "project_id" UUID NOT NULL,
+    "task_type_id" UUID,
+    "type" "TaskTypeCode" NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "project_task_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "time_sheets" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "project_id" UUID NOT NULL,
+    "project_task_type_id" UUID,
+    "stamp_date" DATE NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "end_date" TIMESTAMP(3) NOT NULL,
+    "exclude_seconds" INTEGER,
+    "total_seconds" INTEGER NOT NULL,
+    "detail" VARCHAR(500),
+    "remark" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "time_sheets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "time_sheet_summaries" (
+    "user_id" UUID NOT NULL,
+    "project_id" UUID NOT NULL,
+    "year" INTEGER NOT NULL,
+    "month" INTEGER NOT NULL,
+    "total_seconds" INTEGER NOT NULL,
+    "stamp_at" TIMESTAMP(3) NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "time_sheet_summaries_pkey" PRIMARY KEY ("user_id","project_id","year")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_code_key" ON "users"("code");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_position_level_id_fkey" FOREIGN KEY ("position_level_id") REFERENCES "position_levels"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "users" ADD CONSTRAINT "users_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "roles" ADD CONSTRAINT "roles_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "roles" ADD CONSTRAINT "roles_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "role_permissions" ADD CONSTRAINT "role_permissions_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "role_permissions" ADD CONSTRAINT "role_permissions_pms_code_fkey" FOREIGN KEY ("pms_code") REFERENCES "m_permissions"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "role_permissions" ADD CONSTRAINT "role_permissions_module_code_fkey" FOREIGN KEY ("module_code") REFERENCES "m_modules"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "m_modules_permissions" ADD CONSTRAINT "m_modules_permissions_pms_code_fkey" FOREIGN KEY ("pms_code") REFERENCES "m_permissions"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "m_modules_permissions" ADD CONSTRAINT "m_modules_permissions_module_code_fkey" FOREIGN KEY ("module_code") REFERENCES "m_modules"("code") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teams" ADD CONSTRAINT "teams_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "teams" ADD CONSTRAINT "teams_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_leaders" ADD CONSTRAINT "team_leaders_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "team_leaders" ADD CONSTRAINT "team_leaders_team_id_fkey" FOREIGN KEY ("team_id") REFERENCES "teams"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "positions" ADD CONSTRAINT "positions_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "positions" ADD CONSTRAINT "positions_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "position_levels" ADD CONSTRAINT "position_levels_position_id_fkey" FOREIGN KEY ("position_id") REFERENCES "positions"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_created_by_fkey" FOREIGN KEY ("created_by") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_updated_by_fkey" FOREIGN KEY ("updated_by") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "project_members" ADD CONSTRAINT "project_members_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "project_members" ADD CONSTRAINT "project_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "project_task_types" ADD CONSTRAINT "project_task_types_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "project_task_types" ADD CONSTRAINT "project_task_types_task_type_id_fkey" FOREIGN KEY ("task_type_id") REFERENCES "task_types"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "time_sheets" ADD CONSTRAINT "time_sheets_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "time_sheets" ADD CONSTRAINT "time_sheets_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "time_sheets" ADD CONSTRAINT "time_sheets_project_task_type_id_fkey" FOREIGN KEY ("project_task_type_id") REFERENCES "project_task_types"("id") ON DELETE SET NULL ON UPDATE CASCADE;
