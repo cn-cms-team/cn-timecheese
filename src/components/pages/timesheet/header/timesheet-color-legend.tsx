757b5b12de438f1d8901326b8a4dab36f1907b35
keerati.y <keerati.y@clicknext.co.th>
2026-03-06 19:09:50 +0700

    fix: update color for 'ไม่ได้ลงเวลา' legend and adjust component styling

--- src/components/pages/timesheet/header/timesheet-color-legend.tsx
+++ src/components/pages/timesheet/header/timesheet-color-legend.tsx
@@ -12,7 +12,7 @@ const TimeSheetColorLegend = () => {
     },
     {
       label: 'ไม่ได้ลงเวลา',
-      color: '#ff0000',
+      color: '#ff4d50',
     },
     // {
     //   label: 'วันหยุด',
@@ -21,11 +21,11 @@ const TimeSheetColorLegend = () => {
   ];
 
   return (
-    <div className="bg-[#f4f4f4] rounded-full py-2 px-3 w-full lg:max-w-md ">
+    <div className="bg-[#f4f4f4] rounded-md py-2 px-3 w-full lg:max-w-md ">
       <div className="flex justify-around items-start md:items-center flex-wrap md:flex-nowrap  gap-4">
         {legends.map((legend, index) => (
-          <div key={index} className="flex  items-center gap-1 text-nowrap text-xs md:text-base">
-            <div className="rounded-full w-3 h-3" style={{ backgroundColor: legend.color }} />
+          <div key={index} className="flex items-center gap-2 text-nowrap text-xs md:text-base">
+            <div className="rounded-md w-3 h-3" style={{ backgroundColor: legend.color }} />
             {legend.label}
           </div>
         ))}
